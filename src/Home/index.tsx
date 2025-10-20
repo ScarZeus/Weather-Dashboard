6f83ce91a9c71a32850febb86927017df80dded0
K Kevin Gladson <kevingladson2427@gmail.com>
2025-10-20 15:39:05 +0530

    GeoLocaction
    Latitude and logitude setup is complete

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,12 +1,52 @@
-import { Button } from "../components/ui/button";
+import { useEffect, useState } from "react";
+import type { GeoLocation } from "../models/GeoLocation";
 
 export default function Home(){
 
-    const
+    const[geoLocation, setGeolocation] = useState<GeoLocation>({
+        lat: null,
+        lon:null,
+        error: null
+    })
+
+     useEffect(() => {
+    if (!navigator.geolocation) {
+      setGeolocation({
+        lat: null,
+        lon: null,
+        error: 'Geolocation is not supported by your browser.',
+      });
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setGeolocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+          error: null,
+        });
+        //logic to get the climate using lat and lon
+      },
+      (err) => {
+        setGeolocation({
+          lat: null,
+          lon: null,
+          error: err.message,
+        });
+      }
+    );
+  }, []);
 
     return(
         <div className="dash-board">
-
+                {geoLocation.error ? (
+                    <p>Error: {geoLocation.error}</p>
+                ) : geoLocation.lat && geoLocation.lon ? (
+                    <p>
+                    Latitude: {geoLocation.lat}, Longitude: {geoLocation.lon}
+                    </p>
+                ) : null}
         </div>
     );
 }
